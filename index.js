1c4e81f43abbe6c0ca1992e5423ba7db61acecdd
Your Name <golamsarwar1896@gmail.com>
2024-12-04 16:36:18 +0600

    secured the database username and password using dotenv and .env file. And hidden using gitignore.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -7,12 +8,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-//eqisports_admin
-//pLfe3zPrjpBowAzr
-
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri =
-  "mongodb+srv://eqisports_admin:pLfe3zPrjpBowAzr@cluster0.i16dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i16dm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
